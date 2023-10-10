9511807267186fd30a8f6425615e08f9938e68ab
Kostya <prol4588@gmail.com>
2023-10-10 23:25:21 +0700

    add dist to gitignore

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.refreshTokenLifeTime = exports.tokenLifeTime = void 0;
-exports.tokenLifeTime = '14m';
-exports.refreshTokenLifeTime = '30d';
-//# sourceMappingURL=constants.js.map
